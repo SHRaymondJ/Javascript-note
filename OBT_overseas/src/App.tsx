2cde9069670e9a625fe702ac9a75b61e9200324c
RaymondJ <396945399@qq.com>
2021-05-24 17:56:18 +0800

    hw

--- OBT_overseas/src/App.tsx
+++ OBT_overseas/src/App.tsx
@@ -5,7 +5,7 @@ import {
     Route,
 } from 'react-router-dom'
 import Login from './pages/login'
-import Home from './pages/home/Homepage'
+import Home from './pages/home'
 import ResetPassword from './pages/resetPassword/ResetPassword'
 
 const App = () => {
