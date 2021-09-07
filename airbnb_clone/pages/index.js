75d5874b74e743073b1579ddd4dc6049b0e381c5
Raymond <396945399@qq.com>
2021-09-07 21:10:11 +0800

    initial

---
+++ airbnb_clone/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Airbnb</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>HELLO WORLD</h1>
+      
+      {/* Banner */}
+    </div>
+  )
+}
