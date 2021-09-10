09b8c78b3c620279d82fe643a221f8329ae9505a
Raymond <396945399@qq.com>
2021-09-10 21:32:39 +0800

    airbnb

---
+++ airbnb_clone/components/Footer.js
@@ -0,0 +1,58 @@
+function Footer() {
+    return (
+        <div className="divide-y md:divide-y-0 divide-gray-600 grid grid-cols-1 md:grid-cols-4 bg-gray-100 px-16 md:px-20 lg:px-32 py-14">
+            <div className="text-gray-600 space-y-3 py-10 md:py-0">
+                <h1 className="text-sm font-bold text-gray-900">ABOUT</h1>
+                <div className="grid grid-cols-3 md:space-y-3 md:block">
+                    <p>How Airbnb works</p>
+                    <p>Newsroom</p>
+                    <p>Airbnb 2021</p>
+                    <p>Investors</p>
+                    <p>Airbnb Plus</p>
+                    <p>Airbnb Luxe</p>
+                    <p>HotelTonight</p>
+                    <p>Airbnb for Work</p>
+                    <p>Made possible by Hosts</p>
+                    <p>Careers</p>
+                    <p>Founders' Letter</p>
+                </div>
+            </div>
+            <div className="text-gray-600 space-y-3 py-10 md:py-0">
+                <h1 className="text-sm font-bold text-gray-900">COMMUNITY</h1>
+                <div className="grid grid-cols-3 md:space-y-3  md:block">
+
+                    <p>Diversity & Belonging</p>
+                    <p>Accessibility</p>
+                    <p>Airbnb Associates</p>
+                    <p>Host Afghan refugees</p>
+                    <p>Guest Referrals</p>
+                    <p>Airbnb.org</p>
+                </div>
+            </div>
+            <div className="text-gray-600 space-y-3 py-10 md:py-0">
+                <h1 className="text-sm font-bold text-gray-900">HOST</h1>
+                <div className="grid grid-cols-3 md:space-y-3  md:block">
+
+                    <p>Host your home</p>
+                    <p>Host an Online Experience</p>
+                    <p>Host an Experience</p>
+                    <p>Responsible hosting</p>
+                    <p>Resource Centre</p>
+                    <p>Community Centre</p>
+                </div>
+            </div>
+            <div className="text-gray-600 space-y-3 py-10 md:py-0">
+                <h1 className="text-sm font-bold text-gray-900">SUPPORT</h1>
+                <div className="grid grid-cols-3 md:space-y-3  md:block">
+                    <p>Our COVID-19 Response</p>
+                    <p>Help Centre</p>
+                    <p>Cancellation options</p>
+                    <p>Neighbourhood Support</p>
+                    <p>Trust & Safety</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
