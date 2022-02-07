e623ca12ffa2b327f170f91375db937e4363a4bf
RaymondJ <396945399@qq.com>
2022-02-07 19:56:38 +0800

    re-format

--- Raymond-blog/components/Article.tsx
+++ Raymond-blog/components/Article.tsx
@@ -6,22 +6,24 @@ import marked from 'marked'
 import '../style/markdown.scoped.scss'
 
 const Article = observer(() => {
-  const { articleId } = useParams<{ articleId: string }>()
-  const articleStore = useArticleStore()
+    const { articleId } = useParams<{ articleId: string }>()
+    const articleStore = useArticleStore()
 
-  const [file, setFile] = useState('')
-  ;(async () => {
-    const path = await articleStore.getActiveArticle(+articleId)
-    const result = await fetch(path).then(
-      (result) => result.text()
+    const [file, setFile] = useState('')
+    ;(async () => {
+        const path = await articleStore.getActiveArticle(+articleId)
+        const result = await fetch(path).then((result) => result.text())
+        const markdown = marked(result)
+        setFile(markdown)
+    })()
+
+    return (
+        <div className="px-4 sm:px-16">
+            {file && (
+                <article dangerouslySetInnerHTML={{ __html: file }}></article>
+            )}
+        </div>
     )
-    const markdown = marked(result)
-    setFile(markdown)
-  })()
-
-  return (
-    <div className="px-4 sm:px-16">{file && <article dangerouslySetInnerHTML={{ __html: file }}></article>}</div>
-  )
 })
 
 export default Article
